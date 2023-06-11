ac2fb72ee92ef92191a85519f58556ef08b06318
isaiasg09 <isaiashsobral@gmail.com>
2023-06-11 18:27:58 -0300

    made success screen design and functionality

--- script.js
+++ script.js
@@ -3,25 +3,30 @@ function handleSubmit(event) {
   const form = event.target;
   const data = new FormData(form);
   const value = data.get("email");
-  console.log(value);
 
   if (
     !value ||
     /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value) === false
   ) {
-    const errorSpanElemetn = document.getElementById("error_span");
+    const errorSpanElement = document.getElementById("error_span");
     const inputElement = document.getElementById("email");
 
-    errorSpanElemetn.classList.remove("hidden");
+    errorSpanElement.classList.remove("hidden");
 
     inputElement.classList.add("error_email");
     inputElement.value = "";
 
     setTimeout(() => {
       inputElement.classList.remove("error_email");
-      errorSpanElemetn.classList.add("hidden");
+      errorSpanElement.classList.add("hidden");
     }, 3000);
   } else {
-    
+    const successElement = document.querySelector(".success");
+    const signUpElement = document.querySelector(".sign-up");
+    const emailSpanElement = document.getElementById("email_span");
+
+    successElement.classList.remove("hidden");
+    signUpElement.classList.add("hidden");
+    emailSpanElement.innerHTML = value;
   }
 }
